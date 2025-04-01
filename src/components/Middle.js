c5907fe6006abb9665950871de2caffab8762a43
Shivendra Kumar Sonkar <60128893+Shivendra-99@users.noreply.github.com>
2025-04-01 13:34:09 +0530

    Update Middle.js

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -8,12 +8,12 @@ const Middle = (props) => {
         let { category, pageSize } = props;
         async function fetchData() {
             if (props.category === null) {
-                const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_VERCEL_API_KEY}&pagesize=${pageSize}`;
+                const url = `https://cors-everywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_VERCEL_API_KEY}&pagesize=${pageSize}`;
                 let dat = await fetch(url);
                 let jsonData = await dat.json();
                 setdata(jsonData.articles);
             } else {
-                const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.REACT_APP_VERCEL_API_KEY}&pagesize=${pageSize}`;
+                const url = `https://cors-everywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.REACT_APP_VERCEL_API_KEY}&pagesize=${pageSize}`;
                 let dat = await fetch(url);
                 let jsonData = await dat.json();
                 setdata(jsonData.articles);
